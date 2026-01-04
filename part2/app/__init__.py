20bd519a19c9b7b4401175ef969dd3b895fa9375
root <root@ip-10-42-106-193.ec2.internal>
2026-01-04 17:28:08 +0000

    fix

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,19 +1,16 @@
 from flask import Flask
 from flask_restx import Api
-from app.presentation.api.v1.users import api as users_ns
-from app.presentation.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
+    api = Api(app)
 
-    api = Api(
-        app,
-        title="HBnB API",
-        version="1.0",
-        doc="/"
-    )
+    from app.presentation.api.v1.users import api as users_ns
+    from app.presentation.api.v1.amenities import api as amenities_ns
+    from app.presentation.api.v1.places import api as places_ns
 
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(places_ns, path="/api/v1/places")
 
     return app
